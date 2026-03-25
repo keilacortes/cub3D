90c9d269ae67c77564c5cf0820b7c6687c075110
keilacortes <keilawonka4@gmail.com>
2026-03-25 13:59:49 -0300

    feat:(ft_printf): ft_printf accepts fd argument

--- libs/libft/inc/ft_printf.h
+++ libs/libft/inc/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: kqueiroz <kqueiroz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 13:56:57 by kqueiroz          #+#    #+#             */
-/*   Updated: 2026/02/08 01:14:17 by kqueiroz         ###   ########.fr       */
+/*   Updated: 2026/03/25 13:41:47 by kqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-int	ft_printf(const char *format, ...);
+int	ft_printf(int fd, const char *format, ...);
 int	pf_putchar_fd(char c, int fd);
 int	pf_putstr_fd(char *s, int fd);
 int	pf_putnbr_fd(int n, int fd);
--- libs/libft/src/ft_printf/ft_printf.c
+++ libs/libft/src/ft_printf/ft_printf.c
@@ -6,35 +6,32 @@
 /*   By: kqueiroz <kqueiroz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 15:16:28 by kqueiroz          #+#    #+#             */
-/*   Updated: 2026/02/08 01:18:24 by kqueiroz         ###   ########.fr       */
+/*   Updated: 2026/03/25 13:52:27 by kqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	check_format(char format, va_list args)
+static int	check_format(char format, va_list *args, int fd)
 {
-	int	fd;
-
-	fd = 1;
 	if (format == '%')
 		return (pf_putchar_fd('%', fd));
 	else if (format == 'c')
-		return (pf_putchar_fd(va_arg(args, int), fd));
+		return (pf_putchar_fd(va_arg(*args, int), fd));
 	else if (format == 's')
-		return (pf_putstr_fd(va_arg(args, char *), fd));
+		return (pf_putstr_fd(va_arg(*args, char *), fd));
 	else if (format == 'p')
-		return (pf_putptr_fd(va_arg(args, void *), fd));
+		return (pf_putptr_fd(va_arg(*args, void *), fd));
 	else if (format == 'd' || format == 'i')
-		return (pf_putnbr_fd(va_arg(args, int), fd));
+		return (pf_putnbr_fd(va_arg(*args, int), fd));
 	else if (format == 'u')
-		return (pf_putunsigned_fd(va_arg(args, unsigned int), fd));
+		return (pf_putunsigned_fd(va_arg(*args, unsigned int), fd));
 	else if (format == 'x' || format == 'X')
-		return (pf_puthex_fd(va_arg(args, unsigned int), format, fd));
+		return (pf_puthex_fd(va_arg(*args, unsigned int), format, fd));
 	return (0);
 }
 
-int	ft_printf(const char *format, ...)
+int	ft_printf(int fd, const char *format, ...)
 {
 	va_list	args;
 	int		count;
@@ -43,15 +40,15 @@ int	ft_printf(const char *format, ...)
 	count = 0;
 	while (*format)
 	{
-		if (*format == '%')
+		if (*format == '%' && *(format + 1))
 		{
 			format++;
-			count += check_format(*format, args);
+			count += check_format(*format, &args, fd);
 		}
 		else
-			count += write(1, format, 1);
+			count += write(fd, format, 1);
 		format++;
 	}
-	va_end (args);
+	va_end(args);
 	return (count);
 }

0272844a83caea9345ecb74e73c782ffd44dd700
keilacortes <keilawonka4@gmail.com>
2026-03-25 15:55:23 -0300

    fix:norminette

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: kqueiroz <kqueiroz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 13:51:06 by kqueiroz          #+#    #+#             */
-/*   Updated: 2026/03/25 14:07:09 by kqueiroz         ###   ########.fr       */
+/*   Updated: 2026/03/25 15:09:01 by kqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 int	main(int argc, char **argv)
 {
-	if (argc != 2)
-		exit_error("Usage: ./cub3d <file.cub>");
 	void	*mlx;
 	void	*win;
 
+	if (argc != 2)
+		exit_error("Usage: ./cub3d <file.cub>");
 	check_arg(argv[1]);
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 800, 600, "cub3D");

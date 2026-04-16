409c05de18354069cda507816189d1b07c0987c9
Keila Queiroz Cortes <kqueiroz@c1r3p3.42sp.org.br>
2026-04-16 17:30:07 -0300

    fix norminette problem

--- srcs/parse/validate_tex.c
+++ srcs/parse/validate_tex.c
@@ -6,13 +6,33 @@
 /*   By: kqueiroz <kqueiroz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/29 09:09:11 by kqueiroz          #+#    #+#             */
-/*   Updated: 2026/04/05 07:51:01 by kqueiroz         ###   ########.fr       */
+/*   Updated: 2026/04/16 17:29:44 by kqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void check_file_tex(t_textures *tex)
+void	horizontal(t_textures *tex)
+{
+	int	fd;
+
+	fd = open(tex->west, O_RDONLY);
+	if (fd < 0)
+	{
+		free(tex->west);
+		exit_error("Could not open west texture file");
+	}
+	close(fd);
+	fd = open(tex->east, O_RDONLY);
+	if (fd < 0)
+	{
+		free(tex->east);
+		exit_error("Could not open east texture file");
+	}
+	close(fd);
+}
+
+void	check_file_tex(t_textures *tex)
 {
 	int	fd;
 
@@ -30,18 +50,5 @@ void check_file_tex(t_textures *tex)
 		exit_error("Could not open south texture file");
 	}
 	close(fd);
-	fd = open(tex->west, O_RDONLY);
-	if (fd < 0)
-	{
-		free(tex->west);
-		exit_error("Could not open west texture file");
-	}
-	close(fd);
-	fd = open(tex->east, O_RDONLY);
-	if (fd < 0)
-	{
-		free(tex->east);
-		exit_error("Could not open east texture file");
-	}
-	close(fd);
+	horizontal(tex);
 }
